5c4223ffaa95734d2cb69280fc7a8bc544ab2147
Sargun <soniakamalj@gmail.com>
2024-04-20 03:55:24 +0530

    first commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const NewsItem = (props) => {
+
+  
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+      <div className="my-3">
+        <div className="card" >
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}> {source} </span>
+          <img src={!imageUrl ? "https://static.toiimg.com/thumb/msid-94122264,width-800,resizemode-4,imgsize-8060/share.jpg" : imageUrl} className="card-img-top" alt="..." />
+          <div className="card-body">
+
+            <h5 className="card-title">{title}</h5>
+
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-muted">By {!author ? "unknown" : author} on {new Date(date).toGMTString()}</small></p>
+            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+          </div>
+        </div>
+      </div>
+    )
+  
+}
+
+export default NewsItem
